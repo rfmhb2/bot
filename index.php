add46415350c623e07a6bfe9951434a5de8691ba
mohammad hassan bolivand <mohammadhassanbolivand@gmail.com>
2018-10-05 16:56:15 +0330

    Update index.php

--- index.php
+++ index.php
@@ -5,5 +5,4 @@ $contents = json_decode($contents);
 
 $link = "https://api.telegram.org/bot623045896:AAFT89eXGayhB7G6wlIEty-01vSuRmjXDgI/sendMessage?text=".json_encode($contents)."&chat_id=32862694";
 
-$link = "https://api.telegram.org/bot623045896:AAFT89eXGayhB7G6wlIEty-01vSuRmjXDgI/sendMessage?text=test&chat_id=32862694";
 echo "reserve:".file_get_contents($link);
