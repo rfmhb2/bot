771b3bfc907b71a72b1a375b92304e5b04d7a2c2
mohammad hassan bolivand <mohammadhassanbolivand@gmail.com>
2018-10-05 18:36:55 +0330

    Update index.php
    fix bugs

--- index.php
+++ index.php
@@ -13,5 +13,5 @@ if (isset($chat_id) && isset($text)) {
     $telegram->sendMessage($chat_id, $text);
 }
 $update = $telegram->getWebhookUpdates();
-//$chat_id = $update->message->chat->id;
-$telegram->sendMessage(32862694, json_encode($update));
+$chat_id = $update['message']['chat']['id'];
+$telegram->sendMessage($chat_id, $chat_id);
