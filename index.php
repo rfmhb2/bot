712a6005af99b196200579e297a4ef7d2d552e36
mohammad hassan bolivand <mohammadhassanbolivand@gmail.com>
2018-10-05 16:48:15 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,4 @@
-    <?php
-    echo "tnx hero";
+<?php 
+
+$link = "https://api.telegram.org/bot623045896:AAFT89eXGayhB7G6wlIEty-01vSuRmjXDgI/sendMessage?text=test&chat_id=32862694";
+echo "reserve:".file_get_contents($link);
